0e172ff2c262c12d6a79f9aa71802e3dd4e2d600
Ridwan <aladebaruwa@gmail.com>
2023-07-29 13:09:54 +0100

    add

--- string.js
+++ string.js
@@ -1,2 +1,7 @@
 const string = 'This is a string';
 console.log(string);
+
+const sglDbl = 'Would you eat a "fish supper"?';
+const dblSgl = "I'm feeling blue.";
+console.log(sglDbl);
+console.log(dblSgl);
