d94616a2357f81f61554593ecd6eda9f0c4908ff
Ridwan <aladebaruwa@gmail.com>
2023-07-29 14:31:17 +0100

    add

--- string.js
+++ string.js
@@ -40,3 +40,11 @@ console.log(`${g2}, ${n2}`);
 const str = "page";
 const num = 404;
 console.log (`${str} ${num}`);// page 404
+
+const song = "Fight the Youth";
+const score = 9;
+const highestScore = 10;
+const output = `I like the song ${song}. I gave it a score of ${
+  (score / highestScore) * 100
+}%.`;
+console.log(output); // "I like the song Fight the Youth. I gave it a score of 90%."
