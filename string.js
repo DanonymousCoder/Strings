e598c0cd14e6fa6f806e0c68352aed36d3211944
Ridwan <aladebaruwa@gmail.com>
2023-07-30 22:09:28 +0100

    add

--- string.js
+++ string.js
@@ -48,3 +48,5 @@ const output = `I like the song ${song}. I gave it a score of ${
   (score / highestScore) * 100
 }%.`;
 console.log(output); // "I like the song Fight the Youth. I gave it a score of 90%."
+
+
