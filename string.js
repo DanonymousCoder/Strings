ae52a513d3dd960cdd3bdeee1029f8e407643426
Ridwan <aladebaruwa@gmail.com>
2023-07-30 22:25:46 +0100

    add

--- string.js
+++ string.js
@@ -68,3 +68,7 @@ let newText = text3.replace("Microsoft", "W3Schools");
 
 let text4 = "Hello World!";       // String
 let text5 = text5.toLowerCase();  // text2 is text1 converted to lower
+
+let text1 = "Hello";
+let text2 = "World";
+let text3 = text1.concat(" ", text2);
