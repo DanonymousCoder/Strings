a94daad7da64d6503edee205b702e0bf7021fa8b
Ridwan <aladebaruwa@gmail.com>
2023-07-31 11:34:11 +0100

    add

--- string.js
+++ string.js
@@ -107,5 +107,5 @@ let char2 = text21[0];
 
 // split()
 let txt = Ridwanullah, Sakiinah, Abdsalam;
-let split = txt.split(",");
+let result = txt.split(",");
 document.getElementById('demo').innerHTML = split[1]
