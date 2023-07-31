e145f2b4d0bd6bc15f40918a3d9da6fe0526fb9f
Ridwan <aladebaruwa@gmail.com>
2023-07-31 12:27:11 +0100

    add

--- string.js
+++ string.js
@@ -108,4 +108,10 @@ let char2 = text21[0];
 // split()
 let txt = Ridwanullah, Sakiinah, Abdsalam;
 let result = txt.split(",");
-document.getElementById("demo").innerHTML = result[1]
+document.getElementById("demo").innerHTML = result[1];
+
+const five = 5;
+const ten = 10;
+console.log(`Fifteen is ${five + ten} and not ${2 * five + ten}.`);
+// "Fifteen is 15 and not 20."
+
