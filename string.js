388cbc18953b2eb91a038dbc8256b867b35ff82e
Ridwan <aladebaruwa@gmail.com>
2023-07-31 12:28:24 +0100

    add

--- string.js
+++ string.js
@@ -114,4 +114,22 @@ const five = 5;
 const ten = 10;
 console.log(`Fifteen is ${five + ten} and not ${2 * five + ten}.`);
 // "Fifteen is 15 and not 20."
+// July 17, 2014 00:00:00 UTC:
+const july172014 = new Date("2014-07-17");
+
+const options = {
+  year: "2-digit",
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+  timeZoneName: "short",
+};
+const americanDateTime = new Intl.DateTimeFormat("en-US", options).format;
+
+// Local timezone vary depending on your settings
+// In CEST, logs: 07/17/14, 02:00 AM GMT+2
+// In PDT, logs: 07/16/14, 05:00 PM GMT-7
+console.log(americanDateTime(july172014));
+
 
